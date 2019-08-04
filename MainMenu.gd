07a62da63c84767c07d120c4289b8ea61d3199ef
MrCdK <contact@mrcdk.com>
2019-08-04 13:37:18 +0200

    Oh

--- MainMenu.gd
+++ MainMenu.gd
@@ -2,6 +2,7 @@ extends PanelContainer
 
 func _ready():
 	Music.play_music(preload("res://assets/brightly-fancy-by-kevin-macleod.ogg"))
+	print(Engine.get_version_info())
 
 func _on_PlayButton_pressed():
 	Globals.shuffle_question()
